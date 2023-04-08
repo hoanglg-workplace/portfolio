7433e40c316527932c2412bdcd9af87bde0f38f8
Lý Gia Hoàng <hoanglg6699@gmail.com>
2023-04-09 00:01:03 +0700

    f

---
+++ src/data/WorkData.js
@@ -0,0 +1,97 @@
+export const WorkList = [
+  {
+    id: 1,
+    name: "Shopee Clone (Web Only)",
+    description: "It is a clone of e-commerce platform Shopee",
+    tags: ["HTML", "CSS"],
+    demo: "https://hoanglg6699.github.io/lgh.github.io/ShopeeClone/",
+    github:
+      "https://github.com/hoanglg6699/lgh.github.io/tree/main/ShopeeClone",
+  },
+  {
+    id: 2,
+    name: "Music Player",
+    description:
+      "A music player website build in HTML and JavaScript. Enjoy it.",
+    tags: ["HTML", "CSS", "JavaScript"],
+    demo: "https://hoanglg6699.github.io/lgh.github.io/MusicPlayer/",
+    github:
+      "https://github.com/hoanglg6699/lgh.github.io/tree/main/MusicPlayer",
+  },
+  {
+    id: 3,
+    name: "Ultimate CV",
+    description: "It is a online CV for my job",
+    tags: ["ReactJS", "CSS", "Antd", "Swiper", "Emailjs-com"],
+    demo: "https://online-cv-hoanglg.vercel.app/",
+    github: "",
+  },
+  {
+    id: 4,
+    name: "Music Player",
+    description:
+      "A music player website build in ReactJS. Full function. Enjoy it.",
+    tags: ["ReactJS", "CSS"],
+    demo: "https://musicplayer-hoangworkplace.vercel.app/",
+    github: "",
+  },
+  {
+    id: 5,
+    name: "WeatherApp",
+    description: "A weather app build using ReactJS.",
+    tags: ["ReactJS", "CSS", "API", "moment"],
+    demo: "https://weatherapp-hoanglg.vercel.app/",
+    github: "",
+  },
+  {
+    id: 6,
+    name: "NoNameProject (Web Only)",
+    description: "This is a big project",
+    tags: [
+      "ReactJS",
+      "UmiJS",
+      "CSS",
+      "LESS",
+      "axios",
+      "Firebase",
+      "moment",
+      "...",
+    ],
+
+    demo: "https://nonameproject.vercel.app/",
+    github: "",
+  },
+  {
+    id: 7,
+    name: "Lofi",
+    description:
+      "A music player website build in ReactJS. Full function. Enjoy it.",
+    tags: ["ReactJS", "CSS", "styled-components"],
+
+    demo: "https://lofi-hoangworkplace.vercel.app/",
+    github: "",
+  },
+  {
+    id: 8,
+    name: "Ancruising Rotation",
+    description:
+      "",
+    tags: ["ReactJS", "CSS"],
+
+    demo: "https://ancruising-hoangworkplace.vercel.app/",
+    github: "",
+  },
+  {
+    id: 9,
+    name: "The Gym",
+    description:
+      "",
+    tags: ["ReactJS", "CSS"],
+
+    demo: "https://thegym-hoangworkplace.vercel.app/",
+    github: "",
+  },
+
+  
+  
+];
